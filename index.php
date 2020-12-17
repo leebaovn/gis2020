dacb3204e6a5522a85a40619542c363fd7bc75d0
Lee Bao <leebao101@gmail.com>
2020-12-17 23:50:42 +0700

    gis

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
+<?php
+session_start();
+?>
 <html>
-
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
@@ -17,8 +19,8 @@
 
   <link rel="stylesheet" href="https://js.arcgis.com/4.17/esri/themes/light/main.css">
   <script src="https://js.arcgis.com/4.17/"></script>
-    <link rel="stylesheet" href="./style.css">
-  <script>
+  <link rel="stylesheet" href="./style.css">
+  <script defer>
     require([
       "esri/Map",
       "esri/views/MapView",
@@ -29,6 +31,40 @@
       "esri/widgets/Slider",
       "esri/tasks/support/MultipartColorRamp",
     ], function (Map, MapView, RouteTask, RouteParameters, FeatureSet, Graphic, Slider, MultipartColorRamp) {
+      function drawPoint(point) {
+        var graphic = new Graphic({
+          symbol: {
+            type: "simple-marker",
+            color: "gray",
+            size: "8px"
+          },
+          geometry: point
+        });
+        view.graphics.add(graphic);
+      }
+
+      function drawLine(paths) {
+        // drawPoint(x);
+        // drawPoint(y);
+        console.log('imhere')
+        var polyline = {
+          type: "polyline",  
+            paths: paths
+        };
+
+        var polylineSymbol = {
+          type: "simple-line",
+          color: 'black',
+          width: 4
+        };
+
+        var polylineGraphic = new Graphic({
+          geometry: polyline,
+          symbol: polylineSymbol
+        });
+
+        view.graphics.add(polylineGraphic);
+      }
 
       var map = new Map({
         basemap: "topo-vector"
@@ -61,25 +97,70 @@
       // });
       // xmlHttp.send("function=add_new_arc" + nodeList);
 
-      
-      
-
+      let isSelectRoute = false;
+      let paths = []
+      let ok = false;
       view.on("click", function (event) {
+        const {longitude,latitude} = event.mapPoint;
         if (view.graphics.length === 0) {
           addGraphic("start", event.mapPoint);
-          document.getElementById("start").value = `${event.mapPoint.longitude},${event.mapPoint.latitude}`;
+          document.getElementById("start").value = `${longitude},${latitude}`;
         } else if (view.graphics.length === 1) {
           addGraphic("finish", event.mapPoint);
-          document.getElementById("destination").value = `${event.mapPoint.longitude},${event.mapPoint.latitude}`;
-
+          document.getElementById("destination").value = `${longitude},${latitude}`;
+          isSelectRoute = true;
+          document.getElementById("status").innerHTML = 'Hãy chọn lộ trình di chuyển của bạn';
           getRoute();
-        } else {
-          view.graphics.removeAll();
-          addGraphic("start", event.mapPoint);
+        } else{
+          if(view.graphics.length > 6) {
+            if(!ok){
+              drawLine(paths)
+              ok = true;
+              const {length} = paths;
+               const slider = new Slider({
+                container: "sliderDiv",
+                min: 0,
+                max: length,
+                values: [ length ],
+                snapOnClickEnabled: false,
+                visibleElements: {
+                  labels: true,
+                  rangeLabels: true
+                },
+              });
+
+              slider.tickConfigs = [{
+                mode: "count",
+                values: length,
+                labelsVisible: true,
+                tickCreatedFunction: function(initialValue, tickElement, labelElement) {
+                  labelElement.innerHTML = 't' + labelElement["data-value"];
+                  // tickElement.classList.add("largeTicks");
+                  // labelElement.classList.add("largeLabels");
+                  labelElement.onclick = function() {
+                    const newValue = labelElement["data-value"];
+                    slider.values = [ newValue ];
+                  };
+                }
+              }];
+              view.ui.add(slider);
+            }
+
+
+
+             return;
+            }
+          drawPoint(event.mapPoint)
+          paths.push([longitude,latitude])
+          
         }
+        
+        // else {
+        //   view.graphics.removeAll();
+        //   addGraphic("start", event.mapPoint);
+        // }
       });
-
-
+      
       function addGraphic(type, point) {
         var graphic = new Graphic({
           symbol: {
@@ -93,10 +174,10 @@
       }
 
       function getRoute() {
-        // Setup the route parameters
+       
         var routeParams = new RouteParameters({
           stops: new FeatureSet({
-            features: view.graphics.toArray() // Pass the array of graphics
+            features: view.graphics.toArray()
           }),
           returnDirections: true
         });
@@ -118,40 +199,63 @@
 
       }
 
-      const slider = new Slider({
-        container: "sliderDiv",
-        min: 0,
-        max: 10,
-        values: [ 10 ],
-        snapOnClickEnabled: false,
-        visibleElements: {
-          labels: true,
-          rangeLabels: true
-        },
-      });
+      
 
-      slider.tickConfigs = [{
-        mode: "count",
-        values: 11,
-        labelsVisible: true,
-        tickCreatedFunction: function(initialValue, tickElement, labelElement) {
-          labelElement.innerHTML = 't' + labelElement["data-value"];
-          // tickElement.classList.add("largeTicks");
-          // labelElement.classList.add("largeLabels");
-          labelElement.onclick = function() {
-            const newValue = labelElement["data-value"];
-            slider.values = [ newValue ];
-          };
-        }
-      }];
-      view.ui.add(slider);
+      // const slider = new Slider({
+      //   container: "sliderDiv",
+      //   min: 0,
+      //   max: 10,
+      //   values: [ 10 ],
+      //   snapOnClickEnabled: false,
+      //   visibleElements: {
+      //     labels: true,
+      //     rangeLabels: true
+      //   },
+      // });
+
+      // slider.tickConfigs = [{
+      //   mode: "count",
+      //   values: 11,
+      //   labelsVisible: true,
+      //   tickCreatedFunction: function(initialValue, tickElement, labelElement) {
+      //     labelElement.innerHTML = 't' + labelElement["data-value"];
+      //     // tickElement.classList.add("largeTicks");
+      //     // labelElement.classList.add("largeLabels");
+      //     labelElement.onclick = function() {
+      //       const newValue = labelElement["data-value"];
+      //       slider.values = [ newValue ];
+      //     };
+      //   }
+      // }];
+      // view.ui.add(slider);
     });
+   
   </script>
+  
 </head>
 
 <body>
+    <div class="status" id="status">Chọn điểm đi và điểm đến</div>
+    <div class='modal' id='modal'>
+      <div class="overlay"></div>
+      <div class="vehicle-form" id='vehicle-form'>
+        <form action="" method='POST'>
+          <div class="field">
 
+            <label for="id">Biển số xe</label><input type="text" name='id' id='id'>
+          </div>
+          <div class="field">
+
+            <label for="color">Màu xe</label><input type="color" name='color' id='color'>
+          </div>
+          <div class="field">
+            <button type='submit'>Đăng ký</button>
+          </div>
+        </form>
+      </div>
+      </div>
 <div class="container">
+    
     <div class="header">
     <div class='header__info'>
       <label>Điểm đi</label>
@@ -175,4 +279,12 @@
 </div>
 </body>
 
+<?php
+  $_SESSION['vehicle']='12312';
+    if(isset($_SESSION['vehicle'])){
+      echo '<script type="text/javascript">
+      document.getElementById("modal").style.display="none";
+      </script>';
+    }
+?>
 </html>
